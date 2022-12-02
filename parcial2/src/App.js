5c796a98b483af0b811b81823033186332b28006
veronica <v.escobar1@uniandes.edu.co>
2022-12-02 08:13:18 -0500

    P1: State hook para almacentar informacion de las bandas. Effect Hook para hacer el fetch de un archivo json (use axios)

--- parcial2/src/App.js
+++ parcial2/src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  //State for bands
+  const [bands, setBands] = useState([]);
+
+  //State for selected band
+  const [selectedBand, setSelectedBand] = useState(null);
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const fetchInfo = async () => {
+    //URL Bandas
+    const url = "https://gist.githubusercontent.com/josejbocanegra/806a4dcd1af61b4cc498d24c52e84320/raw/8711b7af9091d2831ed043563cad2a61311b0a5f/music-bands.json";
+    //Fetch Data
+    const response = await axios.get(url);
+    //Data content
+    const data = response.data;
+    //Set data
+    setBands(data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
